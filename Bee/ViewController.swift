f83bb884469d4c335c1ba13a2822485767c8dec7
Fabian Canas <fabian@mojotech.com>
2015-06-05 13:33:28 -0400

    Initial commit

---
+++ Bee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bee
+//
+//  Created by Fabian Canas on 6/5/15.
+//  Copyright (c) 2015 MojoTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
